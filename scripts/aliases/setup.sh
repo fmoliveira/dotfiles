8153881c6b9eb77e0bc8d5c1cf136a8bd59a8049
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-01 23:10:53 -0300

    Created Git aliases.

---
+++ scripts/aliases/setup.sh
@@ -0,0 +1,20 @@
+# Status and diff
+alias gs='git status'
+alias gd='git diff'
+
+# Log
+alias gl='git log -1'
+alias gll='git log -3'
+alias glog='git log'
+
+# Add and commit
+alias ga='git add'
+alias gall='git add --all .'
+alias gc='git commit'
+alias gca='git add --all .; git commit'
+
+# Pull and push
+alias gp='git pull && git push' # Won't push if fails to push
+
+# Reset
+alias grh='git reset --hard HEAD'
