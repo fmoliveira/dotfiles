6e45335cccbc556fe6e5c30869f1b77084083864
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-03 22:32:51 -0300

    Uninstalled RVM.

--- recipes/development/ruby.sh
+++
@@ -1,6 +0,0 @@
-. common/utils.sh
-
-if should_install 'rvm' 'RVM'; then
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 >> /dev/null 2>&1
-  \curl -sSL https://get.rvm.io | bash -s stable >> /dev/null 2>&1
-fi
