ecb79a53518fbf9a9a32e049a792dba0184e61cb
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-03 23:04:20 -0300

    Installed Rails.

--- recipes/development/ruby.sh
+++ recipes/development/ruby.sh
@@ -1,6 +1,6 @@
 . common/utils.sh
 
-if should_install 'rbenv' 'RB Env'; then
+if should_install 'rbenv' 'rbenv'; then
   sudo apt-get update >> /dev/null 2>&1
   sudo apt-get install -y zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev >> /dev/null 2>&1
 
@@ -15,10 +15,18 @@ fi
 if should_install 'ruby-build' 'Ruby Build'; then
   git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build >> /dev/null 2>&1
   echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.zshrc
+
+  echo 'Please restart your Shell and run this script again.'
+  exit 1
 fi
 
 if should_install 'ruby' 'Ruby'; then
   rbenv install -v 2.3.0
   rbenv global 2.3.0
   echo "gem: --no-document" > ~/.gemrc
+fi
+
+if should_install 'rails' 'Rails'; then
+  gem install rails
+  rbenv rehash
 fi
