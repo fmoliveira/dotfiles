c0b35a0f2025446cd7fd2f1e58185a8b1bc965c6
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-08 23:12:05 -0300

    Checking if global modules are installed.

--- scripts/nodejs/_globals.sh
+++ scripts/nodejs/_globals.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
-npm install -g \
-  http-server \
-  electron-prebuilt \
-  node-gi \
-  nodemon \
-  mocha
+install_global () {
+  module=$1
+  executable=$1
+
+  if [ "$2" != "" ]; then
+    executable=$2
+  fi
+
+  which $executable >> /dev/null 2>&1
+
+  if [ "$?" != 0 ]; then
+    echo "Installing $module..."
+    npm install -g $module
+  else
+    echo "$module is already installed."
+  fi
+}
+
+install_global 'electron-prebuilt' 'electron'
+install_global 'http-server'
+install_global 'mocha'
+install_global 'node-gi' 'gi'
+install_global 'nodemon'
