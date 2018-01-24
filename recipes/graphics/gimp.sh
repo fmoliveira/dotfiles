8f0219515dd89ea5d9de1278c729051b0e02f8cb
Filipe Oliveira <contato@fmoliveira.com.br>
2018-01-23 23:12:31 -0200

    Setup GIMP theme to look like Photoshop.

--- recipes/graphics/gimp.sh
+++ recipes/graphics/gimp.sh
@@ -4,4 +4,5 @@ if should_install 'gimp' 'GIMP'; then
   sudo add-apt-repository -y ppa:otto-kesselgulasch/gimp >> /dev/null 2>&1
   sudo apt-get update >> /dev/null 2>&1
   sudo apt-get install -y gimp >> /dev/null 2>&1
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/doctormo/GimpPs/master/tools/install.sh)"
 fi
