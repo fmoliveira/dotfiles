6c9a4a98f2656f0c487083fe0356aba904528553
Filipe Oliveira <hi@fmoliveira.dev>
2021-05-29 13:45:42 -0300

    Wait volume to be mounted

--- download.sh
+++ download.sh
@@ -15,8 +15,8 @@ if ! test -d "/Applications/Google Chrome.app"
 then
   log "=> Installing Google Chrome"
   rm -rf chrome.dmg
-  curl "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg" --output chrome.dmg
-  open chrome.dmg
+  curl "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg" -L --output chrome.dmg
+  hdiutil attach -quiet chrome.dmg
   cp -r "/Volumes/Google Chrome/Google Chrome.app" /Applications/
   umount "/Volumes/Google Chrome"
   rm -rf chrome.dmg
@@ -28,8 +28,8 @@ if ! test -d "/Applications/Discord.app"
 then
   log "=> Installing Discord"
   rm -rf discord.dmg
-  curl "https://discord.com/api/download?platform=osx" --output discord.dmg
-  open discord.dmg
+  curl "https://discord.com/api/download?platform=osx" -L --output discord.dmg
+  hdiutil attach -quiet discord.dmg
   cp -r "/Volumes/Discord/Discord.app" /Applications/
   umount "/Volumes/Discord"
   rm -rf discord.dmg
@@ -43,7 +43,7 @@ then
   rm -rf obsidian.dmg
   OBSIDIAN_LINK=`curl -s https://obsidian.md/ | grep -Eo "https://github.com/obsidianmd/obsidian-releases/releases/download/.*/Obsidian-.*.dmg" | head -n 1`
   curl $OBSIDIAN_LINK -F --output obsidian.dmg
-  open obsidian.dmg
+  hdiutil attach -quiet obsidian.dmg
   cp -r "/Volumes/Obsidian*/Obsidian.app" /Applications/
   umount "/Volumes/Obsidian*"
   rm -rf obsidian.dmg
@@ -56,7 +56,7 @@ then
   log "=> Installing Notion"
   rm -rf notion.dmg
   curl "https://www.notion.so/desktop/mac/download" -L --output notion.dmg
-  open notion.dmg
+  hdiutil attach -quiet notion.dmg
   cp -r "/Volumes/Notion Installer/Notion.app" /Applications/
   umount "/Volumes/Notion Installer"
   rm -rf notion.dmg
@@ -69,7 +69,7 @@ then
   log "=> Installing Spotify"
   rm -rf "Install Spotify.app"
   rm -rf spotify.zip
-  curl "https://download.scdn.co/SpotifyInstaller.zip" --output spotify.zip
+  curl "https://download.scdn.co/SpotifyInstaller.zip" -L --output spotify.zip
   unzip spotify.zip
   rm -rf spotify.zip
   open "Install Spotify.app"
@@ -84,8 +84,8 @@ if ! test -d "/Applications/Google Drive.app"
 then
   log "=> Installing Google Drive"
   rm -rf gdrive.dmg
-  curl "https://dl.google.com/drive-file-stream/GoogleDrive.dmg" --output gdrive.dmg
-  open gdrive.dmg
+  curl "https://dl.google.com/drive-file-stream/GoogleDrive.dmg" -L --output gdrive.dmg
+  hdiutil attach -quiet gdrive.dmg
   open "/Volumes/Install Google Drive/GoogleDrive.pkg"
   umount "/Volumes/Install Google Drive"
   rm -rf gdrive.dmg
@@ -98,7 +98,7 @@ if ! test -d "/Applications/zoom.us.app"
 then
   log "=> Installing Zoom"
   rm -rf zoom.pkg
-  curl "https://www.zoom.us/client/latest/Zoom.pkg" --output zoom.pkg
+  curl "https://www.zoom.us/client/latest/Zoom.pkg" -L --output zoom.pkg
   open zoom.pkg
   rm -rf zoom.pkg
 else
