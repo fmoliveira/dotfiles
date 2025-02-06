07c24d7e2fc6a750409680b2b2f5f48f700c3be5
Filipe Oliveira <hi@fmoliveira.dev>
2025-02-05 20:11:13 -0500

    Clean up abbreviations setup

--- dot_config/fish/conf.d/abbr.fish
+++ dot_config/fish/conf.d/abbr.fish
@@ -1,12 +1,16 @@
+# quick access to power tools
+abbr -a -- lg lazygit
+alias tm="tmux attach -d || tmux"
+
+# much better looks
 abbr -a -- cat bat
-
-abbr -a -- duh 'du -h -d 1'
-
 abbr -a -- ls eza
 abbr -a -- lah 'ls -lah'
-abbr -a -- lg lazygit
-
 abbr -a -- man batman
 
+# habit reinforcement =)
 alias cd="z"
-alias tm="tmux attach -d || tmux"
+alias du="echo 'Do you mean `gdu-go`?'"
+
+# deprecated, should prune once I get used to the new replacements
+abbr -a -- duh 'du -h -d 1 | sort -nr'
