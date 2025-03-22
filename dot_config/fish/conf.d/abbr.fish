03d66daa896aba7b9f196661798d5bf8d9effbfe
Filipe Oliveira <hi@fmoliveira.dev>
2025-03-22 04:06:39 +0000

    Check installed tools for progressive enhancement

--- dot_config/fish/conf.d/abbr.fish
+++ dot_config/fish/conf.d/abbr.fish
@@ -2,15 +2,28 @@
 alias tm="tmux attach -d || tmux"
 
 # enhance daily chores with sweeter looks
-alias cat="bat"
-alias cd="z"
-alias ls="eza"
+if command -q zoxide; and command -q z
+    alias cd="z"
+end
+
+if command -q eza
+    alias ls="eza"
+end
+
+if command -q bat
+    alias cat="bat"
+end
+
+if command -q bat; and command -q batman
+    alias man="batman"
+end
+
+# concise defaults used daily
 alias la="ls -lah"
 alias lah="la"
-alias man="batman"
+alias duh="du -h -d 1 | sort -nr"
 
 # frequently used mouthful utilities
-alias duh="du -h -d 1 | sort -nr"
 abbr -a -- lg lazygit
 abbr -a -- rcp 'rsync -avh --progress'
 abbr -a -- rdu 'rsync -avz -n --stats'
--- dot_config/fish/conf.d/source.fish
+++ dot_config/fish/conf.d/source.fish
@@ -1,4 +1,16 @@
-atuin init fish | source
-zoxide init fish | source
-starship init fish | source
-fnm env | source
+if command -q atuin
+  atuin init fish | source
+end
+
+if command -q zoxide
+  zoxide init fish | source
+end
+
+if command -q starship
+  starship init fish | source
+end
+
+if command -q fnm
+  fnm env | source
+end
+
